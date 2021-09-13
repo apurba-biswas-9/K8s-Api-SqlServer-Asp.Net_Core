b2a5b3cd3f027f42d0e478d42270fb1c52e0abb0
shilpamandal85 <shilpamandal81@gmail.com>
2021-09-13 13:04:59 +0530

    first commit

---
+++ API/testApiK8s/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testApiK8s.Models;
+
+namespace testApiK8s.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly TestContext _dbContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, TestContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+       
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+
+            _dbContext.TestTables.Add(new TestTable {  Name =  "Apurba"   }); 
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           
+           return Ok(_dbContext.TestTables.Select(s => s).ToList());
+        }
+    }
+}
---
+++ API/testApiK8s/Models/TestContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace testApiK8s.Models
+{
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TestTable> TestTables { get; set; }
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TestTable>(entity =>
+            {
+                entity.ToTable("TestTable");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
